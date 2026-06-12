496057d7f65822434ac7ca41dc785a8486cec6b2
Thara <pstharadevi@gmail.com>
2026-06-12 15:59:21 +0530

    Add unit tests

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,7 +1,7 @@
 from src.math_operation import add, sub
 def test_add():
-    assert add(2,3)==5
-    assert(-1,1)==0
+    assert add(-1,1) == 0
+    assert sub(2,3) == -1
 
 def test_sub():
    assert sub(5,3)==2
